ef3356279cea9d5a73a29f6e8a39fe52f3f977eb
wjdtpdud00 <152463268+wjdtpdud00@users.noreply.github.com>
2024-03-14 09:45:18 +0900

    search_tourplace

--- src/main/java/com/semi/gold/controller/TourPlaceController.java
+++ src/main/java/com/semi/gold/controller/TourPlaceController.java
@@ -1,30 +1,41 @@
 package com.semi.gold.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.semi.gold.model.dto.TourplaceSearchDTO;
 import com.semi.gold.model.vo.TourPlace;
 import com.semi.gold.service.TourplaceService;
 
+@RequestMapping("/tourplace")
 @Controller
 public class TourPlaceController {
 	
 	@Autowired
 	private TourplaceService service;
 	
-	@GetMapping("/tourplace")
-	public String tourplace(Model model) {
-		List<TourPlace> list = service.showTourplace();
-		model.addAttribute("list", list);
+	@GetMapping("/search")
+	private String tourplace(Model model) {
 		return "/tourplace/tourlist"; 
 	}
-	
-	
-	
+
+	@ResponseBody
+	@RequestMapping("/getSearchList")
+	private List<TourPlace> getSearchList(@RequestBody TourplaceSearchDTO request){
+		
+		List<TourPlace> returnList = service.searchTourplace(request);
+		return returnList;
+	}	
+
+
 	
 	
 	
