0a42a4c0be378d5c35bdfcc1bbc0bb416e5ad18f
wjdtpdud00 <152463268+wjdtpdud00@users.noreply.github.com>
2024-03-26 09:08:54 +0900

    restaurantDetail

---
+++ src/main/java/com/semi/gold/model/dto/RestaurantDetailSearchDTO.java
@@ -0,0 +1,10 @@
+package com.semi.gold.model.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor
+public class RestaurantDetailSearchDTO {
+	private String contentId;
+	
+}
