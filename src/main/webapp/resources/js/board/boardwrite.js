8ee49f979cde21f87bb91dfc53bae9cbb150506f
JeongHoeYeong <JeongHoeYeong@users.noreply.github.com>
2024-03-26 17:26:00 +0900

    03/26

--- src/main/webapp/resources/js/board/boardwrite.js
+++ src/main/webapp/resources/js/board/boardwrite.js
@@ -2,18 +2,31 @@ function checkBoard() {
   if ($.trim($("#boardTitle").val()).length == 0) {
     alert("제목을 입력하세요.");
     return false;
+  } else if ($.trim($("#boardTitle").val()).length > 20) {
+  	alert("제목은 20자까지 입력할 수 있습니다.");
+  	return false;
   } else if ($.trim($("#boardContent").val()).length == 0) {
     alert("내용을 입력하세요.");
     return false;
   } else if ($.trim($("#category").val()).length == 0) {
     alert("카테고리를 선택하세요.");
     return false;
-  } else {
-    if(confirm("게시물을 수정 하시겠습니까?")) {
-    	alert("게시물 수정 완료되었습니다.")
-    	return true;
-    } else {
-   	return false;
-    }
-  }
+  } 
+  return true;
+}
+
+function editBoard() {
+	if(checkBoard()) {
+	return confirm("게시물을 수정 하시겠습니까?");
+	} else {
+		return false;
+	}
+}
+
+function writeBoard() {
+	if(checkBoard()) {
+	return confirm("게시물을 등록 하시겠습니까?");
+	} else {
+		return false;
+	}
 }
